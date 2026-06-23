7dae1357e5f0062718d14900a8d05e3b80faf729
Carlos Armando Martinez Meza <likzss2@outlook.com>
2026-06-22 23:20:15 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cs
@@ -0,0 +1,6 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        return new string(s.OrderBy(c => c).ToArray()) ==
+               new string(t.OrderBy(c => c).ToArray());
+    }
+}
